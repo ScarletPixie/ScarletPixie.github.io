bf545f90414226cba3de9a2dea93420f27912c16
cranberry <scarletskynned@gmail.com>
2025-05-17 18:08:48 -0300

    feat: add hamburger menu

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,6 +2,7 @@ const toggle = document.querySelector(".navbar__toggle");
 const menu = document.querySelector(".navbar__menu");
 const menuLinks = document.querySelectorAll(".navbar__link");
 const navBrand = document.querySelector(".navbar__brand");
+const toggleIcon = document.querySelector(".navbar__menu-icon")
 
 
 // TOGGLE NAVBAR MENU ON/OFF
@@ -9,6 +10,7 @@ toggle.addEventListener("click", () => {
     const expanded = toggle.getAttribute("aria-expanded") === "true";
     toggle.setAttribute("aria-expanded", String(!expanded));
     menu.classList.toggle("active");
+    setMenuIcon(expanded);
 });
 
 
@@ -21,4 +23,13 @@ menuLinks.forEach((lnk) => {
 function closeMenu() {
     toggle.setAttribute("aria-expanded", "false");
     menu.classList.remove("active");
+    setMenuIcon(false);
+}
+
+function setMenuIcon(mode)
+{
+    if (mode === true)
+        toggleIcon.src = "../images/menu/burger-menu.svg";
+    else
+        toggleIcon.src = "../images/menu/x-symbol.svg";
 }
