cbafbd862ae94c6c229979782b73e40b1a6ff898
ScarletPixie <ScarletPixie@users.noreply.github.com>
2025-05-26 14:49:26 +0000

    deploy: 955f3131a719351b8958db8cf12e9b0e4c6238ee

---
+++ README.md
@@ -0,0 +1,4 @@
+# Welcome to my portifolio
+<p>
+This is where i plan on showing my greatest achievements and what i'm working on.
+</p>
---
+++ scripts/navbar.js
@@ -0,0 +1,35 @@
+const toggle = document.querySelector(".navbar__toggle");
+const menu = document.querySelector(".navbar__menu");
+const menuLinks = document.querySelectorAll(".navbar__link");
+const navBrand = document.querySelector(".navbar__brand");
+const toggleIcon = document.querySelector(".navbar__menu-icon")
+
+
+// TOGGLE NAVBAR MENU ON/OFF
+toggle.addEventListener("click", () => {
+    const expanded = toggle.getAttribute("aria-expanded") === "true";
+    toggle.setAttribute("aria-expanded", String(!expanded));
+    menu.classList.toggle("active");
+    setMenuIcon(expanded);
+});
+
+
+// CLOSE MENU AFTER CLICKING ANY LINK
+navBrand.addEventListener("click", closeMenu);
+menuLinks.forEach((lnk) => {
+    lnk.addEventListener("click", closeMenu);
+});
+
+function closeMenu() {
+    toggle.setAttribute("aria-expanded", "false");
+    menu.classList.remove("active");
+    setMenuIcon(true);
+}
+
+function setMenuIcon(mode)
+{
+    if (mode === true)
+        toggleIcon.src = "../images/menu/burger-menu.svg";
+    else
+        toggleIcon.src = "../images/menu/x-symbol.svg";
+}
