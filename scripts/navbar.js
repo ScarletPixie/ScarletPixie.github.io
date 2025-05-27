6a0d413998edb9c773c09b64be50cf6dee4fd2e3
cranberry <scarletskynned@gmail.com>
2025-05-27 11:53:44 -0300

    feat: add hide on scroll down feature to navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,8 +1,25 @@
+const navbar = document.querySelector(".navbar");
 const toggle = document.querySelector(".navbar__toggle");
 const menu = document.querySelector(".navbar__menu");
 const menuLinks = document.querySelectorAll(".navbar__link");
 const navBrand = document.querySelector(".navbar__brand");
 const toggleIcon = document.querySelector(".navbar__menu-icon")
+
+
+
+// HIDE/SHOW ON SCROLL
+const sensibility = 75;
+let lastScroll = window.scrollY;
+window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY;
+    if (currentScroll > lastScroll && currentScroll > sensibility)
+        navbar.classList.add("hidden");
+    else
+        navbar.classList.remove("hidden");
+    lastScroll = currentScroll;
+});
+
+
 
 
 // TOGGLE NAVBAR MENU ON/OFF
