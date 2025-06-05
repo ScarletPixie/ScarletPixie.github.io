6c6ca7414196249b1761ba8beda013167f73ce02
ScarletPixie <ScarletPixie@users.noreply.github.com>
2025-06-05 01:58:24 +0000

    deploy: 5d5bc411877fb366780146ac034f5a2ece36ef69

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -11,7 +11,7 @@ const sensibility = 175;
 let lastScroll = window.scrollY;
 window.addEventListener("scroll", () => {
     const currentScroll = window.scrollY;
-    if (!menu.classList.contains("active") && currentScroll > lastScroll && currentScroll > sensibility)
+    if (noNavElementFocus() && currentScroll > lastScroll && currentScroll > sensibility)
         navbar.classList.add("hidden");
     else
         navbar.classList.remove("hidden");
@@ -21,6 +21,7 @@ window.addEventListener("scroll", () => {
 
 
 // TOGGLE NAVBAR MENU ON/OFF
+toggle.addEventListener("focus", showNav);
 toggle.addEventListener("click", () => {
     const expanded = toggle.getAttribute("aria-expanded") === "true";
     toggle.setAttribute("aria-expanded", String(!expanded));
@@ -31,18 +32,44 @@ toggle.addEventListener("click", () => {
 
 // CLOSE MENU AFTER CLICKING ANY LINK
 navBrand.addEventListener("click", closeMenu);
+navBrand.addEventListener("focus", showNav);
 menuLinks.forEach((lnk) => {
     lnk.addEventListener("click", closeMenu);
-    lnk.addEventListener("focus", () => {
-        navbar.classList.remove("hidden");
-        menu.classList.add("active");
-    })
+    lnk.addEventListener("focus", showNav);
+    lnk.addEventListener("focus", openMenu);
 });
 
-function closeMenu() {
+
+// CLOSE MENU IF INNER CHILD WAS SELECTED AND LOST FOCUS
+menu.addEventListener("focusout", closeMenu);
+navbar.addEventListener("focusout", closeMenu);
+
+
+// HELPERS
+function noNavElementFocus()
+{
+    // TODO
+    return false;
+    // return !(navbar.contains(document.activeElement) === document.body);
+}
+
+
+// CALLBACKS
+function showNav()
+{
+    navbar.classList.remove("hidden");
+}
+function closeMenu()
+{
     toggle.setAttribute("aria-expanded", "false");
     menu.classList.remove("active");
     setMenuIcon(true);
+}
+function openMenu()
+{
+    toggle.setAttribute("aria-expanded", "true");
+    menu.classList.add("active");
+    setMenuIcon(false);
 }
 
 function setMenuIcon(mode)
