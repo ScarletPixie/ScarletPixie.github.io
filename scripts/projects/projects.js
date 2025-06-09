5c288516059f6409585610cb01d8b36d4bc097a2
ScarletPixie <ScarletPixie@users.noreply.github.com>
2025-06-09 14:21:25 +0000

    deploy: 34a97d807d961be642b6c9908361c06e107fed8b

---
+++ scripts/projects/projects.js
@@ -0,0 +1,13 @@
+export const PROJECT_LIST =
+    [
+        {
+            title: "Country Info",
+            url: "https://scarletpixie.github.io/country-info",
+            preview: {
+                summary: "A simple project about fetching data from an external API",
+                thumbnail: "./img/country-info/dark-theme-home-page.png",
+            },
+            images: [],
+            description: "lorem ipsum",
+        }
+    ]
