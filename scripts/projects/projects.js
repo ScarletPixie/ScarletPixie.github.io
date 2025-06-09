278646c76a8de1b3bb8290124a25b80139956063
akino <scarletskynnned@gmail.com>
2025-06-09 19:12:39 -0300

    feat: add template for project card

--- scripts/projects/projects.js
+++ scripts/projects/projects.js
@@ -9,5 +9,75 @@ export const PROJECT_LIST =
             },
             images: [],
             description: "lorem ipsum",
-        }
+        },
+        {
+            title: "Country Info",
+            url: "https://scarletpixie.github.io/country-info",
+            preview: {
+                summary: "A simple project about fetching data from an external API",
+                thumbnail: "./img/country-info/dark-theme-home-page.png",
+            },
+            images: [],
+            description: "lorem ipsum",
+        },
+        {
+            title: "Country Info",
+            url: "https://scarletpixie.github.io/country-info",
+            preview: {
+                summary: "A simple project about fetching data from an external API",
+                thumbnail: "./img/country-info/dark-theme-home-page.png",
+            },
+            images: [],
+            description: "lorem ipsum",
+        },
+        {
+            title: "Country Info",
+            url: "https://scarletpixie.github.io/country-info",
+            preview: {
+                summary: "A simple project about fetching data from an external API",
+                thumbnail: "./img/country-info/dark-theme-home-page.png",
+            },
+            images: [],
+            description: "lorem ipsum",
+        },
+        {
+            title: "Country Info",
+            url: "https://scarletpixie.github.io/country-info",
+            preview: {
+                summary: "A simple project about fetching data from an external API",
+                thumbnail: "./img/country-info/dark-theme-home-page.png",
+            },
+            images: [],
+            description: "lorem ipsum",
+        },
+        {
+            title: "Country Info",
+            url: "https://scarletpixie.github.io/country-info",
+            preview: {
+                summary: "A simple project about fetching data from an external API",
+                thumbnail: "./img/country-info/dark-theme-home-page.png",
+            },
+            images: [],
+            description: "lorem ipsum",
+        },
+        {
+            title: "Country Info",
+            url: "https://scarletpixie.github.io/country-info",
+            preview: {
+                summary: "A simple project about fetching data from an external API",
+                thumbnail: "./img/country-info/dark-theme-home-page.png",
+            },
+            images: [],
+            description: "lorem ipsum",
+        },
+        {
+            title: "Country Info",
+            url: "https://scarletpixie.github.io/country-info",
+            preview: {
+                summary: "A simple project about fetching data from an external API",
+                thumbnail: "./img/country-info/dark-theme-home-page.png",
+            },
+            images: [],
+            description: "lorem ipsum",
+        },
     ]
