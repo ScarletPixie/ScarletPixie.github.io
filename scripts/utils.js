04e61afbfae35eca0bf87fcba3671303de94e93e
cranberry <scarletskynned@gmail.com>
2025-06-23 11:32:58 -0300

    fix: fix typo in object att

--- scripts/utils.js
+++ scripts/utils.js
@@ -20,7 +20,7 @@ export class GlobalElementRect
     #rect = {
         x: 0, y: 0,
         left: 0, right: 0, top: 0, bottom: 0,
-        width: 0, heigh: 0,
+        width: 0, height: 0,
     }
     #element = null
     constructor(element)
