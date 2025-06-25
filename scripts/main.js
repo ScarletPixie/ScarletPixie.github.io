f962774fc453a98bfe8796a4c856038f667fc5d3
cranberry <scarletskynned@gmail.com>
2025-06-25 09:59:35 -0300

    refactor: simplify project card creation and move it to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
-import { Navbar } from "./navbar.js"
-import { Taskbar } from "./footer.js"
+import { Navbar } from "./navbar.js";
+import { Taskbar } from "./footer.js";
 import * as events from "./shared/global-events.js";
+import { CardDragBehavior, PROJECT_LIST, ProjectCardComponent } from "./projects/index.js";
 
 
 // PAGE COMPONENTS SETUP
@@ -14,3 +15,36 @@ taskbar.setup();
 const scrollNotifier = events.GlobalScrollEventNotifier.instance();
 scrollNotifier.subscribe(navbar);
 scrollNotifier.subscribe(taskbar);
+
+
+// CREATE PROJECT CARDS
+const projectList = [];
+const projectListNode = document.querySelector(".project-list");
+PROJECT_LIST.forEach(projectData => {
+    const card = new ProjectCardComponent(projectData);
+    projectList.push(card);
+    card.render(projectListNode);
+});
+
+// SET PROJECT CARD BEHAVIORS
+const activeCardDraggingBehaviors = [];
+projectList.forEach((card) => {
+    const dragBehavior = new CardDragBehavior(card, projectListNode);
+    dragBehavior.setup();
+
+    activeCardDraggingBehaviors.push(dragBehavior);
+
+    //MINIMIZE
+    card.windowButtonsNode.children[0].addEventListener("click", (e) => {
+        e.stopPropagation();
+    });
+    // MAXIMIZE/RESTORE
+    card.windowButtonsNode.children[1].addEventListener("click", (e) => {
+        e.stopPropagation();
+    });
+    // CLOSE
+    card.windowButtonsNode.children[2].addEventListener("click", (e) => {
+        e.stopPropagation();
+        card.destroy();
+    })
+});
