3808b834fd8af4c2049a05ce672233241e3440dd
akino <scarletskynnned@gmail.com>
2025-07-19 19:46:31 -0300

    fix: add relevant links to tech stack

--- scripts/projects/images.js
+++ scripts/projects/images.js
@@ -1,4 +1,5 @@
 let techIcons = null;
+let techLinks = null;
 const techList = {
     javascript: "../images/projects/tech-stack-icons/javascript.svg",
     html5: "../images/projects/tech-stack-icons/html5.svg",
@@ -8,6 +9,15 @@ const techList = {
     python: "../images/projects/tech-stack-icons/python.svg",
     docker: "../images/projects/tech-stack-icons/docker.svg",
 };
+const techLinkList = {
+    javascript: "https://pt.m.wikipedia.org/wiki/JavaScript",
+    html5: "https://pt.m.wikipedia.org/wiki/HTML5",
+    css3: "https://pt.m.wikipedia.org/wiki/CSS3",
+    django: "https://www.djangoproject.com/",
+    djangorestframework: "https://www.django-rest-framework.org/",
+    python: "https://www.python.org/",
+    docker: "https://www.docker.com/",
+}
   
 export function getTechIcons()
 {
@@ -24,4 +34,15 @@ export function getTechIcons()
         techIcons.set(name, img);
     }
     return techIcons;
+}
+export function getTechLinks()
+{
+    if (techLinks)
+        return techLinks;
+    techLinks = new Map();
+    for (const [name, url] of Object.entries(techLinkList))
+    {
+        techLinks.set(name, url);
+    }
+    return techLinks;
 }
